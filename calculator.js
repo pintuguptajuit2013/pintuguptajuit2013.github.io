d1b673ecd92c4424c5e8a335dfb3801cf10c7a43
Pintu Gupta <gpintu061@gmail.com>
2020-06-14 17:10:43 +0530

    Add New Calculator Using Ex CSS and Java Script

---
+++ calculator.js
@@ -0,0 +1,89 @@
+// Apend the Number Clicked on Button
+function getNumber(num){
+	var input_var = document.getElementById('input');
+	switch(num){
+		case 1: input_var.value +='1';
+				break;
+		case 2: input_var.value +='2';
+				break;
+		case 3: input_var.value +='3';
+				break;
+		case 4: input_var.value +='4';
+				break;
+		case 5: input_var.value +='5';
+				break;
+		case 6: input_var.value +='6';
+				break;
+		case 7: input_var.value +='7';
+				break;
+		case 8: input_var.value +='8';
+				break;
+		case 9: input_var.value +='9';
+				break;
+		case 0: input_var.value +='0';
+				break;
+	}
+}
+
+// Bracket Set
+var i=0; 
+function brackets(){
+	var input_var = document.getElementById('input');
+	if(i==0){
+		input_var.value +='(';
+		i=1;
+	}else if(i==1){
+		input_var +=')';
+		i=0;
+		
+	}
+	
+}
+
+// Compute Function 
+
+function compute(){
+	document.getElementById('answer').value = "";
+	var input_var = document.getElementById('input');
+	ans = Math.floor(+eval(input_var.value));
+	document.getElementById('answer').value = '=' + ans;
+}
+
+// Get the operand 
+
+function getOperand(operand){
+	var input_var = document.getElementById('input');
+	switch(operand){
+		case '+':
+			input_var.value +='+';
+			break;
+		case '-':
+			input_var.value +='-';
+			break;
+		case 'x':
+			input_var.value +='*';
+			break;
+		case '/':
+			input_var.value +='/';
+			break;
+		case '+/-':
+			input_var.value +='-'+ input_var.value;
+			break;
+	}
+}
+
+// back space funtion 
+
+function backspace(){
+	var input_var = document.getElementById('input');
+	var x = input_var.value;
+	if(x.length >0){
+		x = x.substring(0,x.length-1); // remove the last character;
+		input_var.value = x;
+	}
+}
+// Clear the Screen
+function clearScreen(){
+	document.getElementById('input').value = "0";
+	document.getElementById('answer').value="";
+}
